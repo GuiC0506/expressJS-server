23938f2e61c9df9395cbbb8eeb6476e88bea32ae
Guilherme Costa <echina725@gmail.com>
2024-01-16 23:52:57 -0300

    feat: post grades endpoint added && get grades refactoring

--- src/index.js
+++ src/index.js
@@ -2,26 +2,45 @@ const express = require("express");
 const app = express();
 const PORT = 3333;
 
-app.get("/grades", (req, res) => {
-    res.send([
-        {
-            name: 'Churros',
-            class: 'A',
-            grade: 8
-        },
-        {
-            name: 'Shoyou',
-            class: 'B',
-            grade: 6
-        },
-        {
-            name: 'Bela',
-            class: 'C',
-            grade: 9
+app.use(
+    express.json(),
+    express.urlencoded(),
+);
 
-        }
-    ])
+app.get("/", (req, res) => {
+    res.send('<h2>welcome user</h2>');
+})
+
+
+var grades = [
+    {
+        name: 'Churros',
+        class: 'A',
+        grade: 8
+    },
+    {
+        name: 'Shoyou',
+        class: 'B',
+        grade: 6
+    },
+    {
+        name: 'Bela',
+        class: 'C',
+        grade: 9
+
+    }
+]
+
+
+app.get("/grades", (req, res) => {
+    res.send(grades)
 });
+
+app.post('/grades', (req, res) => {
+    grades = [...grades, req.body]
+    res.statusCode = 201
+    res.send('Postado com sucesso');
+})
 
 app.listen(PORT, () => {
     console.log(`Express server running on port ${PORT}`);
