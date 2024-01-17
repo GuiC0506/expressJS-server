d6b7dc6816871215a46d2fbe24c568ed6846e4e6
Guilherme Costa <echina725@gmail.com>
2024-01-16 22:09:08 -0300

    feat: get-grades endpoint added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3333;
 
+app.get("/grades", (req, res) => {
+    res.send([
+        {
+            name: 'Churros',
+            class: 'A',
+            grade: 8
+        },
+        {
+            name: 'Shoyou',
+            class: 'B',
+            grade: 6
+        },
+        {
+            name: 'Bela',
+            class: 'C',
+            grade: 9
+
+        }
+    ])
+});
+
 app.listen(PORT, () => {
-   console.log(`Express server running on port ${PORT}`) ;
+    console.log(`Express server running on port ${PORT}`);
 })
 
