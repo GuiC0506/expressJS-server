c66b84580beb1d7c21a9d8d1c8dad46723f71e47
Guilherme Costa <echina725@gmail.com>
2024-01-21 14:52:48 -0300

    put request added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-
 const PORT = process.env.PORT || 3333;
 
 app.use(
@@ -17,6 +16,7 @@ app.get("/", (req, res) => {
     res.status(200).send("Welcome");
 })
 
+// get a specific resource
 app.get("/api/users", (req, res) => {
     const { query: { filter, value } } = req;
     if(!filter && !value) return res.send(users);
@@ -35,18 +35,22 @@ app.get("/api/users/:id", (req, res) => {
     res.status(200).send(userInfo);
 })
 
-
-app.get('/api/products', (req, res, next) => {
-    res.status(200).send("Products");
-    next();
-}, (req, res) => {
-    console.log("After returning the products to the client side");
-});
-
+// creates a new resource
 app.post("/api/users", (req, res) => {
     req.body = {...req.body, id: users.at(users.length - 1).id + 1}
     users.push(req.body);
     res.status(201).send(users.at(users.length - 1));
+})
+
+// updates the whole resource, given a specific ID. Overwrites the resource
+app.put("/api/users/:id", (req, res) => {
+    const userId = parseInt(req.params.id);
+    if(isNaN(userId)) return res.status(400).send("Bad Request");
+
+    const userIndex = users.findIndex(user => user.id === userId);
+    if(userIndex === -1) return res.status(404).send("Not Found");
+    users[userIndex] = { id: userId, ...req.body};
+    return res.sendStatus(204);
 })
 
 app.listen(PORT, () => {
