29b7958f69a7459b794b121b4628ee2215ef13d9
Guilherme Costa <echina725@gmail.com>
2024-01-29 18:55:33 -0300

    authentication in the frontend

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,41 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
-    useEffect(() => {
-        fetch("http://localhost:3333/api/users")
-            .then(response => response.json())
-            .then(data => console.log(data));
-    }, []);
+    const usernameRef = useRef();
+    const pwdRef = useRef();
+
+    const handleSubmission = async (e) => {
+        e.preventDefault();
+        const body  = {
+            username: usernameRef.current.value,
+            password: pwdRef.current.value
+        }
+        
+        try {
+            const response = await axios.post("http://localhost:3333/api/loginjwt", body,
+                {
+                    headers: { "Content-Type":"application/json" },
+                    withCredentials: true
+                }
+            );
+            console.log(response.data);
+        } catch(err) {
+            const { error } = err.response.data;
+            console.log(error);
+        }
+    }
+
     return (
     <>
-      hello
+        <form action="" onSubmit={handleSubmission}>
+            <label htmlFor="name">Username</label>
+            <input type="text" ref={usernameRef} />
+            <label htmlFor="name">password</label>
+            <input type="text" ref={pwdRef}/>
+            <input type="submit" value="Submit"/>
+        </form>
     </>
     )
 }
